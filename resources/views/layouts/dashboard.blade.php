5b89c08f599bc65b99a0031b94fe12e9701d48bb
Basil Alrashed <basil.alrashed@alyaseenagri.com>
2024-01-16 15:11:14 +0300

    updated

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -236,7 +236,7 @@
             </div>
         </header>
         <main style="direction: rtl">
-            <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+            <div class="px-4 sm:px-6 lg:px-8 py-8 w-full mx-auto">
                 <div class="sm:flex sm:justify-between sm:items-center mb-8">
                     {{--                    here title--}}
 
