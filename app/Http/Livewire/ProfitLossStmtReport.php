2993c6b8595f72e15bc0af672e3fb6102a40dd5d
Basil Alrashed <basil.alrashed@alyaseenagri.com>
2025-01-06 11:17:14 +0300

    finished the profit-loss-stmt report

--- app/Http/Livewire/ProfitLossStmtReport.php
+++ app/Http/Livewire/ProfitLossStmtReport.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ProfitLossStmtReport extends Component
@@ -23,6 +24,20 @@ class ProfitLossStmtReport extends Component
     ];
 
     protected $listeners = ['create-report' => 'create_report'];
+
+    public function booted() {
+
+
+        if (Auth::user()->is_active == '0'){
+            return redirect()->route('non-active-user');
+        }
+
+        if ((Auth::user()->user_group && in_array('profit-loss-stmt-report', json_decode(Auth::user()->user_group->report_type))) || Auth::user()->role == 'a'){
+            return;
+        } else {
+            return redirect()->route('dashboard');
+        }
+    }
 
     public function render()
     {
