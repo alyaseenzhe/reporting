1364d08fe5db87c475d2d2045b8d74a68123dddf
alyaseenzhe <zahra@alyaseenagri.com>
2026-05-12 09:45:41 +0300

    wip

---
+++ database/migrations/2026_05_12_061752_add_type_column_to_branch_crop_composition_collection_report.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeColumnToBranchCropCompositionCollectionReport extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('branch_crop_composition_collections', function (Blueprint $table) {
+            $table->string('type')->nullable();
+            $table->foreignId('lead_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('branch_crop_composition_collection_report', function (Blueprint $table) {
+            //
+        });
+    }
+}
